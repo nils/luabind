8451f944faee8caa7f79fe1e200993a13cd1b953
Nils <git@to.nilsschnabel.de>
2013-02-10 20:28:41 +0100

    fixes according to https://svn.boost.org/trac/boost/ticket/6631

--- luabind/detail/get_signature.hpp
+++ luabind/detail/get_signature.hpp
@@ -160,7 +160,8 @@ namespace luabind { namespace detail
 
 #endif // LUABIND_GET_SIGNATURE_HPP_INCLUDED
 
-#elif BOOST_PP_ITERATION_FLAGS() == 1
+#else
+#if BOOST_PP_ITERATION_FLAGS() == 1
 
 	// member functions
 	template<class T, class C BOOST_PP_COMMA_IF(BOOST_PP_ITERATION()) BOOST_PP_ENUM_PARAMS(BOOST_PP_ITERATION(), class A)>
@@ -211,6 +212,7 @@ namespace luabind { namespace detail
 		s += ")";
 	}
 
+	#endif
 #endif
 
 #endif // LUABIND_NO_ERROR_CHECKING
